84c48fc5453db0c3ce7768045bb5cc8fe3486a32
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-03-15 12:20:33 +0000

    Documented TileSet class.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@414 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/doc/doxysrc/annchienta.py
+++ Annchienta/doc/doxysrc/annchienta.py
@@ -1356,6 +1356,54 @@ class Tile(Entity):
         pass
 
 
+## \brief Holds Tiles.
+#
+#  This class is used for holding a number of Tile
+#  Surfaces.
+#
+class TileSet:
+
+    ## \brief Loads a new TileSet.
+    #
+    #  This function will load all Tile Surfaces and Tile
+    #  Side Surfaces in the given directory. The Tile
+    #  Surfaces must be called directory/{n}.png and the Tile
+    #  Side Surfaces directory/side{n}.png, where n is an
+    #  integer starting at 1. The Surfaces with number 0 are
+    #  automatically generated and entirely transparent. For
+    #  an explanation about Surfaces and Side Surfaces, see the
+    #  Tile documentation.
+    #
+    def __init__( directory ):
+        pass
+
+    ## \brief Gets a Surface.
+    #
+    #  \param number Number of the Surface you want to retrieve.
+    #  \return The Surface with the given number.
+    def getSurface( number ):
+        pass
+
+    ## \brief Gets a Side Surface.
+    #
+    #  \param number Number of the Side Surface you want to retrieve.
+    #  \return The Side Surface with the given number.
+    def getSideSurface( number ):
+        pass
+
+    ## \brief Gets the Tile Mask.
+    #
+    #  \return A Mask used for Tile collisions.
+    def getMask():
+        pass
+
+    ## \brief Gets the directory.
+    #
+    #  \return The directory in which these Surfaces are situated.
+    def getDirectory():
+        pass
+
+
 ## \page tutorial1 Tutorial 1: The Basics.
 #
 #  \section t1_section1 Your first game.
