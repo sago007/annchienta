a1de9d88e8e5647b7e9f0de3afe8497772ac7e4a
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-05-21 13:05:59 +0000

    Added decent world music and fixed glitch in world code.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@518 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/games/elegiac_convergence/locations/tetia/baniran.py
+++ Annchienta/games/elegiac_convergence/locations/tetia/baniran.py
@@ -26,15 +26,19 @@ if not partyManager.hasRecord("tetia_met_baniran"):
     sceneManager.speak( baniran, "Aelaan! What the heck have you been doing?" )
     sceneManager.chat( esana, "Do you know this lad?", ["Sure, our hideout is in Tetia. I know pretty much everyone here."] )
     sceneManager.speak( baniran, "I heard you were arrested for murder. I'm glad to see you back. I knew they were wrong, you wouldn't do anything so cruel." )
-    a = sceneManager.chat( baniran, "So, how did they find out you were innocent?", ["Well, actually they didn't...", "They found the real murderer later."] )
+    a = sceneManager.chat( baniran, "So, how did they find out you were innocent?", ["Well, actually they didn't...", "I had an alibi."] )
     if a==0:
-        pass
+        sceneManager.chat( baniran, "What? You escaped? How did you manage to do that?", ["I couldn't do that on my own, Esana helped me out."] )
     else:
-        pass
+        sceneManager.chat( baniran, "I see. So, who is this lovely girl you brought with you?", ["That's Esana."] )
 
-else:
-    pass
+    sceneManager.speak( baniran, "Esana? Aren't you the daughter of Bardolph? I'm sorry for you father. Is there anything I can do?" )
+    sceneManager.speak( esana, "Thanks... But I don't think you can help us... unless you have any idea on who really killed my father?" )
+    sceneManager.speak( baniran, "No, I'm sorry." )
 
+sceneManager.speak( baniran, "Why don't you sit here for a while and enjoy the sun?" )
+partyManager.heal()
+sceneManager.info( "Your health was restored.", None )
 
 sceneManager.move( esana, pp )
 sceneManager.quitDialog()
