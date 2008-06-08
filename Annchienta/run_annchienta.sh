9bedc4612478a799f1fb01ee8e3e6e21b6a5c8fa
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-06-08 19:05:25 +0000

    Started directory restructuring.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@551 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/run_annchienta.sh
+++
@@ -1,2 +0,0 @@
-cd `dirname $0`
-bin/annchienta.bin games/elegiac_convergence/main.py $PWD/modules $PWD/write
--- Annchienta/run_editor.sh
+++
@@ -1,2 +0,0 @@
-cd `dirname $0`
-bin//annchienta.bin games/editor/main.py $PWD/modules $PWD/write
