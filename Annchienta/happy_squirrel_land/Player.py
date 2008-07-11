bdb394d06d5ea36410986292697f750276b07df2
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-07-11 11:35:45 +0000

    Added working gun to player.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@594 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/happy_squirrel_land/Player.py
+++ Annchienta/happy_squirrel_land/Player.py
@@ -1,4 +1,4 @@
-import annchienta, Level
+import math, annchienta, Level
 
 class Player:
 
@@ -10,28 +10,53 @@ class Player:
         self.cacheManager = annchienta.getCacheManager()
         
         # Load sprites...
-        self.sprite = self.cacheManager.getSurface("data/squirrel1.png")
+        self.sprite = self.cacheManager.getSurface("data/player.png")
+        self.gun = self.cacheManager.getSurface("data/player_gun.png")
         
         # Set X and Y
         self.x = float(self.videoManager.getScreenWidth()/2)
         self.y = float(Level.treeUpperY() - self.sprite.getHeight()/2)
         
         self.xScale = 1
+        self.angle = 0
         
     def draw( self ):
     
         self.videoManager.pushMatrix()
+        
+        # Go to main body and draw it
         self.videoManager.translate( int(self.x), int(self.y) )
+        
         self.videoManager.scale( self.xScale, 1 )
         self.videoManager.drawSurface( self.sprite, -self.sprite.getWidth()/2, -self.sprite.getHeight()/2 )
+        
+        # Draw the gun
+        self.videoManager.rotate( self.angle if self.xScale==1 else 180-self.angle )
+        self.videoManager.drawSurface( self.gun, -self.gun.getWidth()/2, -self.gun.getHeight()/2 )
+        
         self.videoManager.popMatrix()
         
     def update( self, ms ):
     
         if self.inputManager.keyDown( annchienta.SDLK_LEFT ):
-            self.xScale = -1
-            self.x -= 0.2*ms
+            if self.x-self.sprite.getWidth()/2>0:
+                self.x -= 0.2*ms
         if self.inputManager.keyDown( annchienta.SDLK_RIGHT ):
-            self.xScale = 1
-            self.x += 0.2*ms
+            if self.x+self.sprite.getWidth()/2<self.videoManager.getScreenWidth():
+                self.x += 0.2*ms
             
+        # Calculate angle.
+        mx = float(self.inputManager.getMouseX())
+        my = float(self.inputManager.getMouseY())
+        dx = mx - self.x
+        dx = 0.0001 if dx==0 else dx
+        dy = my - self.y
+        self.angle = math.degrees( math.atan( dy/dx ) )
+        
+        # Invert angle if we're looking to the left
+        if self.x>mx:
+            self.angle = 180.0 + self.angle
+            self.xScale = -1
+        else:
+            self.xScale = 1
+
