eaa1d069ea46dc0cf2d286f3117462c8d57f6876
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-09-25 14:16:28 +0000

    Added half of a soldier sprite.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@694 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/fall_of_imiryn/locations/facilities/onload.py
+++ Annchienta/fall_of_imiryn/locations/facilities/onload.py
@@ -4,6 +4,6 @@ partyManager = PartyManager.getPartyManager()
 battleManager = BattleManager.getBattleManager()
 annchienta.getAudioManager().playMusic( "music/facilities.ogg")
 annchienta.getVideoManager().setClearColor(147,201,233)
-battleManager.enemiesInMap = ["brainfuck mutant"]
+battleManager.enemiesInMap = ["brainfuck mutant", "soldier"]
 battleManager.background = "images/backgrounds/facilities.png"
 
