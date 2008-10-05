e29e9c1e787d032ea0baeaaaaca94f242f2d7459
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-10-05 20:17:52 +0000

    Added banver ship sprite.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@703 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/fall_of_imiryn/locations/facilities/onload.py
+++ Annchienta/fall_of_imiryn/locations/facilities/onload.py
@@ -3,7 +3,7 @@ import annchienta, PartyManager, BattleManager
 partyManager = PartyManager.getPartyManager()
 battleManager = BattleManager.getBattleManager()
 annchienta.getAudioManager().playMusic( "music/facilities.ogg")
-annchienta.getVideoManager().setClearColor(147,201,233)
+annchienta.getVideoManager().setClearColor(72,68,50)
 battleManager.enemiesInMap = ["brainfuck mutant", "sample 0x4a", "soldier"]
 battleManager.background = "images/backgrounds/facilities.png"
 
