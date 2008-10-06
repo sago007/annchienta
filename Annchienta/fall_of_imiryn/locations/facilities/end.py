6fda694cdd3d0822885e66cfaf4dcc86e9298d46
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-10-06 15:18:07 +0000

    Added facility end scene blueprint.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@706 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/fall_of_imiryn/locations/facilities/end.py
+++ Annchienta/fall_of_imiryn/locations/facilities/end.py
@@ -8,20 +8,25 @@ sceneManager = SceneManager.getSceneManager()
 
 currentMap = partyManager.currentMap
 
+# Create a whole bunch of objects/persons and set them to
+# their positions.
 august = partyManager.player
+august.setPosition( annchienta.Point( annchienta.TilePoint, 8, 12 ) )
 
-# Addobject and stuff...
 march = annchienta.Person( "march", "locations/common/march.xml" )
-avril = annchienta.Person( "avril", "locations/common/avril.xml" )
-
-march.setPosition( annchienta.Point( annchienta.TilePoint, 22, 7 ) )
-avril.setPosition( annchienta.Point( annchienta.TilePoint, 24, 7 ) )
+march.setPosition( annchienta.Point( annchienta.TilePoint, 9, 13 ) )
 currentMap.addObject( march )
-currentMap.addObject( avril )
-sceneManager.initDialog( [august, march, avril] )
 
-sceneManager.move( august, annchienta.Point( annchienta.TilePoint, 23, 7 ) )
-avril.lookAt( august )
+avril = annchienta.Person( "avril", "locations/common/avril.xml" )
+avril.setPosition( annchienta.Point( annchienta.TilePoint, 7, 13 ) )
+currentMap.addObject( avril )
+
+ship = annchienta.StaticObject( "ship", "locations/facilities/ship.xml" )
+ship.setPosition( annchienta.Point( annchienta.TilePoint, 5, 5 ) )
+currentMap.addObject( ship )
+
+# Init our dialog.
+sceneManager.initDialog( [august, march, avril] )
 
 sceneManager.speak( august, "Laustwan?" )
 sceneManager.speak( avril, "Laustwan indeed. I wonder why someone would lock them up? You could just order them to stay, right?" )
@@ -30,13 +35,9 @@ sceneManager.speak( avril, "Doesn't seem like their is something wrong with them
 sceneManager.speak( august, "But why would someone need that many of them?" )
 sceneManager.speak( march, "And why should that person hide them, away from the Jemor continent?" )
 
-sceneManager.fade()
-
-partyManager.addRecord("facilities_saw_laustwan")
-
+# Done. clean up everything.
 sceneManager.quitDialog()
 currentMap.removeObject( march )
 currentMap.removeObject( avril )
-
-partyManager.refreshMap()
+currentMap.removeObject( ship )
 
