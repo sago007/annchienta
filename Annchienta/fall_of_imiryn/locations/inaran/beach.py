8bee1d6cd2a1fce135af762e41a9edd1e0740607
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-10-07 09:15:59 +0000

    Improved beach dialog.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@709 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/fall_of_imiryn/locations/inaran/beach.py
+++ Annchienta/fall_of_imiryn/locations/inaran/beach.py
@@ -26,7 +26,7 @@ if not partyManager.hasRecord( "inaran_intro" ):
     sceneManager.text( "The Empire was built on technology and magic. Then, the Laustwan were discovered.", None )
     sceneManager.text( "The Laustwan were a tribe of apparently human-like creatures, who offered their services to men.", None )
     sceneManager.text( "With their help, the Empire was able to expand their territories, and people and Laustwan were able to live very comfortable lives.", None )
-    sceneManager.text( "Now, the Empire of Imiryn was at the height of it's power. It controlled the entire Jemor continent, and had many colonies on the northern side of the Nupol continent.", None )
+    sceneManager.text( "Now, the Empire of Imiryn was at the height of it's power. It controlled the entire Jemor continent, and has some colonies on the northern side of the Nupol continent.", None )
     sceneManager.text( "This Empire was protected by a very fine and well-trained army. The elite section of this army was called the Fifth Guard.", None )
     sceneManager.text( "Our story handles about three aspiring young warriors, hoping to join this Fifth Guard.", None )
 
@@ -38,8 +38,6 @@ if not partyManager.hasRecord( "inaran_intro" ):
     sceneManager.text( "Hold down the left mouse button to move. Then click March when you are close enough." )
     sceneManager.text( "During the game, press the right mouse button to bring up the menu." )
     partyManager.addRecord( "inaran_intro" )
-
-
 
 sceneManager.quitDialog()
 
