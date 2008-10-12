87c65b9dc157031e0448858b877e4c786c29a34f
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-10-12 19:39:48 +0000

    Worked on sky pirate minigame.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@716 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/fall_of_imiryn/locations/fleet/minigame.py
+++ Annchienta/fall_of_imiryn/locations/fleet/minigame.py
@@ -8,6 +8,7 @@ class GameObject:
         self.videoManager = annchienta.getVideoManager()
         self.pos = position
         self.sprite = sprite
+        self.dir = annchienta.Vector( 0, 0 )
 
     def draw( self ):
 
@@ -23,15 +24,27 @@ class Game:
         self.engine = annchienta.getEngine()
         self.videoManager = annchienta.getVideoManager()
         self.inputManager = annchienta.getInputManager()
+        self.cacheManager = annchienta.getCacheManager()
 
-        # Background surface
+        # Background spriteace
         self.background = annchienta.Surface( "images/backgrounds/sky.png" )
         self.backgroundY = 0.0
+
+        # Create a ship for the player
+        self.ship = GameObject( annchienta.Vector( videoManager.getScreenWidth()/2, videoManager.getScreenHeight()/2 ),
+                                annchienta.Surface( "sprites/ship_small.png" ) )
+
+        # Load sprites into cache
+        self.cacheManager.getSurface("sprites/ship_pirate.png")
+
+        # All enemies
+        self.enemies = []
 
     def run( self ):
 
         self.running = True
         self.lastUpdate = None
+        self.nextEnemySpawn = 0
 
         while( self.inputManager.running() and self.running ):
 
@@ -54,6 +67,44 @@ class Game:
         while( self.backgroundY > self.videoManager.getScreenHeight() ):
             self.backgroundY -= self.videoManager.getScreenHeight()
 
+        # Update player
+        mouse = annchienta.Vector( self.inputManager.getMouseX(), self.inputManager.getMouseY() )
+        mouse -= self.ship.pos
+        mouse.normalize()
+        mouse *= (ms * 0.2)
+        self.ship.pos += mouse
+
+        # Check if we should spawn enemies
+        self.nextEnemySpawn -= ms
+        while self.nextEnemySpawn <= 0:
+            
+            # Spawn a new enemy
+            sprite = self.cacheManager.getSurface("sprites/ship_pirate.png")
+            pos = annchienta.Vector( annchienta.randInt( 0, videoManager.getScreenWidth() ), videoManager.getScreenHeight() + sprite.getHeight() )
+            self.enemies += [ GameObject( pos, sprite ) ]
+            self.nextEnemySpawn += annchienta.randInt( 1000, 2000 )
+
+        # Move enemies
+        for enemy in self.enemies:
+
+            vect = None
+
+            if enemy.pos.y < self.ship.pos.y:
+                vect = annchienta.Vector( enemy.dir )
+            else:
+                vect = self.ship.pos - enemy.pos
+                vect.normalize()
+                enemy.dir = annchienta.Vector( vect )
+
+            vect.y = -1
+
+            vect *= ( ms * 0.2 )
+
+            enemy.pos += vect
+
+        # Remove enemies out of screen
+        self.enemies = filter( lambda e: e.pos.y > -e.sprite.getHeight(), self.enemies )
+
     def draw( self ):
 
         self.videoManager.begin()
@@ -61,6 +112,14 @@ class Game:
         # Draw background
         self.videoManager.drawSurface( self.background, 0, int(self.backgroundY) )
         self.videoManager.drawSurface( self.background, 0, int(self.backgroundY-self.videoManager.getScreenHeight()) )
+
+        # Draw player
+        self.ship.draw()
+
+        # Draw enemies
+        for enemy in self.enemies:
+            enemy.draw()
+
         self.videoManager.end()
 
 
@@ -82,6 +141,10 @@ sceneManager.text( "August:\nAnd so we took Banver's ship in attempt to reach th
 sceneManager.text( "August:\nBut soon we were noticed by these sky pirates Banver mentioned.", None )
 sceneManager.text( "August:\nAt first, it seemed like there weren't too many, so we tried to evade them.", None )
 sceneManager.text( "August:\nBut then...", None )
+
+# Save first
+sceneManager.text( "Info: Your game was saved automatically.", None )
+partyManager.save( "save/save.xml" )
 
 game = Game()
 game.run()
