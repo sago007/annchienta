10f4946e6a6bbf0584990ffa223d453f23e5ce21
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-10-15 17:01:14 +0000

    Finished pirate room scene.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@720 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/fall_of_imiryn/locations/fleet/room.py
+++ Annchienta/fall_of_imiryn/locations/fleet/room.py
@@ -23,11 +23,11 @@ avril = annchienta.Person( "avril", "locations/common/avril.xml" )
 avril.setPosition( annchienta.Point( annchienta.TilePoint, 4, 6 ) )
 currentMap.addObject( avril )
 
-pirate1 = annchienta.Person( "pirate1", "locations/fleet/pirate1.xml" )
+pirate1 = annchienta.Person( "emenver", "locations/fleet/pirate1.xml" )
 pirate1.setPosition( annchienta.Point( annchienta.TilePoint, 7, 5 ) )
 currentMap.addObject( pirate1 )
 
-pirate2 = annchienta.Person( "pirate2", "locations/fleet/pirate2.xml" )
+pirate2 = annchienta.Person( "hoche", "locations/fleet/pirate2.xml" )
 pirate2.setPosition( annchienta.Point( annchienta.TilePoint, 7, 6 ) )
 currentMap.addObject( pirate2 )
 
@@ -41,7 +41,50 @@ avril.lookAt( pirate1 )
 pirate1.lookAt( august )
 pirate2.lookAt( august )
 
-sceneManager.speak( avril, "Yeah, but I treat him right! I don't lock him up." )
+sceneManager.speak( pirate1, "Well, well. Members of the Fifth Guard, in an Empirial Aircraft." )
+sceneManager.speak( pirate1, "Yet, you seem different than the other soldiers who attacked us." )
+sceneManager.move( pirate1, annchienta.Point( annchienta.TilePoint, 7, 3 ), True )
+sceneManager.speak( pirate1, "You fly pretty well, too. You evaded most of fighters." )
+sceneManager.move( pirate1, annchienta.Point( annchienta.TilePoint, 5, 3 ), True )
+sceneManager.speak( pirate1, "It's almost a shame I'll have to kill you." )
+pirate1.lookAt( avril )
+
+sceneManager.speak( august, "You dirty..." )
+sceneManager.speak( avril, "Wait!" )
+sceneManager.move( avril, annchienta.Point( annchienta.TilePoint, 5, 6 ), True )
+sceneManager.speak( avril, "We are allied to the Imiryn Empire no more." )
+
+sceneManager.move( pirate2, annchienta.Point( annchienta.TilePoint, 8, 6 ), True )
+sceneManager.speak( pirate2, "Yeah right. Anyone can say that. We need proof." )
+
+sceneManager.speak( pirate1, "What side are you on, if you're not with the Empire?" )
+sceneManager.speak( march, "We have no side anymore. We are driven by our heart, not by orders." )
+sceneManager.speak( pirate1, "You almost sound like a pirate." )
+
+sceneManager.speak( march, "Then release us! We're not headed to the Empirial City, but to a place north of it." )
+sceneManager.speak( march, "We promise we won't stand in your way." )
+
+sceneManager.speak( pirate2, "We will release you. Aren't we friendly pirates? We only demand one little thing." )
+sceneManager.speak( pirate1, "We want the flight access code of your aircraft to the outer Imiryn Empirial City regions." )
+
+sceneManager.speak( march, "We can't do that... That would be betrayal." )
+sceneManager.speak( pirate2, "Didn't you just say you weren't allied to Imiryn anymore?" )
+sceneManager.speak( march, "Yeah, but..." )
+sceneManager.speak( pirate1, "Would you rather die?" )
+sceneManager.speak( august, "I would've gladly died. But no more. I just saw an Empirial general kill my brother." )
+sceneManager.speak( august, "Besides... Kyzano said what we're fighting for is more important than our Empire." )
+sceneManager.speak( avril, "I agree." )
+
+sceneManager.speak( august, "I couldn't believe it had come this far. Betraying our own Empire.", True )
+sceneManager.speak( august, "I kept telling myself I did this for the greater good, to complete Kyzano's mission.", True )
+sceneManager.speak( august, "After all, he had died for this.", True )
+sceneManager.speak( august, "I kept telling myself I did this for the greater good. Not to save our own dirty little lives...", True )
+
+sceneManager.speak( pirate1, "Fly with us until we reach the Jemor continent. We'll release you, and even give you back your ship." )
+
+sceneManager.fade()
+sceneManager.text( "And so we arrived in Kimen, where we would find the truth, as Kyzano had told us.", None )
+mapManager.stop()
 
 # Done. clean up everything.
 sceneManager.quitDialog()
