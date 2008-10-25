6365ca682771851783cc8e2d375fdfbfe6697c2f
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-10-25 15:56:31 +0000

    Added plantation2 area.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@738 6c58e83f-43c0-4801-a1bd-44083660f6ae

---
+++ Annchienta/fall_of_imiryn/locations/kimen/irrigation.py
@@ -0,0 +1,44 @@
+import annchienta
+import PartyManager, SceneManager, BattleManager
+
+mapManager = annchienta.getMapManager()
+videoManager = annchienta.getVideoManager()
+partyManager = PartyManager.getPartyManager()
+sceneManager = SceneManager.getSceneManager()
+
+currentMap = partyManager.currentMap
+
+august = partyManager.player
+
+sceneManager.initDialog( [august] )
+
+if not partyManager.hasRecord("kimen_inspected_plant"):
+
+    sceneManager.speak( august, "This looks like some sort of irrigation system." )
+
+else:
+
+    numberOfSystem = int( currentMap.getFileName()[-5:-4] )
+
+    if partyManager.hasRecord( "kimen_destroyed_irrigation_"+str(numberOfSystem) ):
+
+        sceneManager.speak( august, "We already sabotaged this irrigation system." )
+
+    else:
+
+        partyManager.addRecord("kimen_destroyed_irrigation_"+str(numberOfSystem))
+
+        numberOfSystemsDestroyed = len( filter( lambda i: partyManager.hasRecord( "kimen_destroyed_irrigation_"+str(i) ), range(1,4) ) )
+
+        if numberOfSystemsDestroyed==1:
+            sceneManager.speak( august, "This seems to be some sort of irrigation system. If we cut this wire..." )
+            sceneManager.text( "You sabotaged the irrigation system!" )
+
+        elif numberOfSystemsDestroyed==2:
+            sceneManager.speak( august, "That makes two of them." )
+
+        else:
+            sceneManager.speak( august, "I guess that was the last one of them. Let's head back to the ship." )
+
+sceneManager.quitDialog()
+partyManager.refreshMap()
