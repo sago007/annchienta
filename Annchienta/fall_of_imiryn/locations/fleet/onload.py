62fad74ccbd35503241db4bb712c7998d3e68cb3
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-11-09 09:06:04 +0000

    Added beautiful beach image.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@773 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/fall_of_imiryn/locations/fleet/onload.py
+++
@@ -1,9 +0,0 @@
-import annchienta, PartyManager, BattleManager
-
-partyManager = PartyManager.getPartyManager()
-battleManager = BattleManager.getBattleManager()
-annchienta.getAudioManager().playMusic( "music/unknown.ogg")
-annchienta.getVideoManager().setClearColor(147,201,233)
-battleManager.enemiesInMap = ["spider", "worm", "ghost"]
-battleManager.background = "images/backgrounds/grass.png"
-
--- Annchienta/fall_of_imiryn/locations/unknown/onload.py
+++ Annchienta/fall_of_imiryn/locations/unknown/onload.py
@@ -5,5 +5,5 @@ battleManager = BattleManager.getBattleManager()
 annchienta.getAudioManager().playMusic( "music/unknown.ogg")
 annchienta.getVideoManager().setClearColor(147,201,233)
 battleManager.enemiesInMap = ["spider", "worm", "ghost"]
-battleManager.background = "images/backgrounds/grass.png"
+battleManager.background = "images/backgrounds/beach.png"
 
