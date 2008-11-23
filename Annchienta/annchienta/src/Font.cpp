0b39b5e008b08129269734200531758c67c7639b
repsa_jih <repsa_jih@6c58e83f-43c0-4801-a1bd-44083660f6ae>
2008-11-23 10:19:13 +0000

    Changed to GPL license.
    git-svn-id: svn://svn.code.sf.net/p/annchienta/code/trunk@804 6c58e83f-43c0-4801-a1bd-44083660f6ae

--- Annchienta/annchienta/src/Font.cpp
+++ Annchienta/annchienta/src/Font.cpp
@@ -1,5 +1,18 @@
-/* This file is part of the Annchienta Project
- * Please consult the license and copyright details in Annchienta/license.txt
+/*  This file is part of Annchienta.
+ *  Copyright 2008 (C) Jasper Van der Jeugt
+ *
+ *  Annchienta is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Annchienta is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with Annchienta.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Font.h"
